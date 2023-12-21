162b27a3b1f5de72bebcb8d88df35cca77a10fa2
Taeke de Haan <taeke.dehaan@live.nl>
2023-12-21 12:05:24 +0100

    [src] add utils and constants

--- src/core.py
+++ src/core.py
@@ -1,87 +1,6 @@
 import pathlib
-
-WAV_EXTENSION = "*.wav"
-AUDIO_FORMAT_OFFSET = 20
-AUDIO_FORMAT_FIELD_SIZE = 2
-WAVE_FORMAT_EXTENSIBLE = 65534
-WAVE_FORMAT_PCM = 1
-
-
-def try_open_file(file: pathlib.Path, mode: str):
-    try:
-        return open(file, mode)
-    except PermissionError as e:
-        print(
-            f"Failed to open file {file.name}: the permision was denied. Caught the following exception:\n{e}"
-        )
-        return None
-    except Exception as e:
-        print(f"Failed to open file {file.name}. Caught the following exception:\n{e}")
-        return None
-
-
-def read_hex_value(file: pathlib.Path, offset: int, field_size: int):
-    maybe_file_handle = try_open_file(file, "rb")
-    if maybe_file_handle is None:
-        return ""
-
-    file_handle = maybe_file_handle
-
-    hex_data: list[str] = []
-
-    # Ignore the data before the specfied offset
-    file_handle.seek(offset)
-
-    # Read the spcified number of hex values starting at the offset
-    for _ in range(field_size):
-        hex_byte = file_handle.read(1).hex()
-
-        if not hex_byte:
-            print(
-                "The provided offset/size goes out of range. Please verify whether these values are correct. If so the file might be corrpted"
-            )
-            break
-
-        hex_data.append(hex_byte)
-
-    file_handle.close()
-
-    if len(hex_data) != AUDIO_FORMAT_FIELD_SIZE:
-        print(
-            "The found hex data does not match the expected field size. File file might be empty or corrupted, skipping this file"
-        )
-        return ""
-
-    # Fix order and convert to hex value
-    hex_data.reverse()
-    return "".join(hex_data).upper()
-
-
-def set_hex_data(file: pathlib.Path, offset: int, field_size: int, hex_value: str):
-    hex_data = [hex_value[i : i + 2] for i in range(0, len(hex_value), 2)]
-
-    if len(hex_data) != AUDIO_FORMAT_FIELD_SIZE:
-        print(
-            "The new hex data does not match the audio format field size. Can not modify the bad files"
-        )
-        return False
-
-    hex_data.reverse()
-
-    # try to open file
-    maybe_file_handle = try_open_file(file, "r+b")
-    if maybe_file_handle is None:
-        return ""
-
-    file_handle = maybe_file_handle
-    file_handle.seek(offset)
-
-    # Overwrite
-    for hex_value in hex_data:
-        file_handle.write(bytes((int(hex_value),)))
-
-    file_handle.close()
-    return True
+import constants
+import utils
 
 
 class Core:
@@ -90,8 +9,8 @@ class Core:
         self._files: list[pathlib.Path] = []
         self._suspicious_files: list[pathlib.Path] = []
 
-        self.suspicious_hex_value = f"{WAVE_FORMAT_EXTENSIBLE:04X}"
-        self.new_hex_value = f"{WAVE_FORMAT_PCM:04X}"
+        self.suspicious_hex_value = f"{constants.WAVE_FORMAT_EXTENSIBLE:04X}"
+        self.new_hex_value = f"{constants.WAVE_FORMAT_PCM:04X}"
 
     def reset(self):
         self._files.clear()
@@ -104,7 +23,7 @@ class Core:
             print("The provided base path {} does not exist")
             return False
 
-        self._files = list(self.base_path.rglob(WAV_EXTENSION))
+        self._files = list(self.base_path.rglob(constants.WAV_EXTENSION))
         if not self._files:
             print(f"Did not find any wav files at {self.base_path}, exitting")
             return False
@@ -127,7 +46,7 @@ class Core:
 
             self._suspicious_files.append(file)
             print(
-                f"The wav file {file.name} contains the key {WAVE_FORMAT_EXTENSIBLE} at the specified location. Storing the file name..."
+                f"The wav file {file.name} contains the key {constants.WAVE_FORMAT_EXTENSIBLE} at the specified location. Storing the file name..."
             )
 
         if not self._suspicious_files:
@@ -138,13 +57,15 @@ class Core:
         return True
 
     def is_wav_file_suspicious(self, file: pathlib.Path):
-        hex_value = read_hex_value(file, AUDIO_FORMAT_OFFSET, AUDIO_FORMAT_FIELD_SIZE)
+        hex_value = utils.read_hex_value(
+            file, constants.AUDIO_FORMAT_OFFSET, constants.AUDIO_FORMAT_FIELD_SIZE
+        )
         if not hex_value:
             print("Failed to read the hex value from the file")
             return False
 
         int_value = int(hex_value, 16)
-        if int_value != WAVE_FORMAT_EXTENSIBLE:
+        if int_value != constants.WAVE_FORMAT_EXTENSIBLE:
             return False
 
         return True
@@ -155,14 +76,14 @@ class Core:
             return False
 
         print(
-            f"Will replace {self.suspicious_hex_value} at offset {AUDIO_FORMAT_OFFSET} and field size {AUDIO_FORMAT_FIELD_SIZE} with {self.new_hex_value}"
+            f"Will replace {self.suspicious_hex_value} at offset {constants.AUDIO_FORMAT_OFFSET} and field size {constants.AUDIO_FORMAT_FIELD_SIZE} with {self.new_hex_value}"
         )
         overall_success = True
         for wav_file in self._suspicious_files:
-            success = set_hex_data(
+            success = utils.set_hex_data(
                 wav_file,
-                AUDIO_FORMAT_OFFSET,
-                AUDIO_FORMAT_FIELD_SIZE,
+                constants.AUDIO_FORMAT_OFFSET,
+                constants.AUDIO_FORMAT_FIELD_SIZE,
                 self.new_hex_value,
             )
             if not success:
---
+++ src/utils.py
@@ -0,0 +1,78 @@
+import pathlib
+
+
+def read_hex_value(file: pathlib.Path, offset: int, field_size: int):
+    maybe_file_handle = try_open_file(file, "rb")
+    if maybe_file_handle is None:
+        return ""
+
+    file_handle = maybe_file_handle
+
+    hex_data: list[str] = []
+
+    # Ignore the data before the specfied offset
+    file_handle.seek(offset)
+
+    # Read the spcified number of hex values starting at the offset
+    for _ in range(field_size):
+        hex_byte = file_handle.read(1).hex()
+
+        if not hex_byte:
+            print(
+                "The provided offset/size goes out of range. Please verify whether these values are correct. If so the file might be corrpted"
+            )
+            break
+
+        hex_data.append(hex_byte)
+
+    file_handle.close()
+
+    if len(hex_data) != field_size:
+        print(
+            f"The found hex data does of length {len(hex_data)} not match the expected field size {field_size}. File file might be empty or corrupted, skipping this file"
+        )
+        return ""
+
+    # Fix order and convert to hex value
+    hex_data.reverse()
+    return "".join(hex_data).upper()
+
+
+def set_hex_data(file: pathlib.Path, offset: int, field_size: int, hex_value: str):
+    hex_data = [hex_value[i : i + 2] for i in range(0, len(hex_value), 2)]
+
+    if len(hex_data) != field_size:
+        print(
+            f"The new hex data of lenth {len(hex_data)} does not match the audio format field size {field_size}. Refusing to set the hex data"
+        )
+        return False
+
+    hex_data.reverse()
+
+    # try to open file
+    maybe_file_handle = try_open_file(file, "r+b")
+    if maybe_file_handle is None:
+        return ""
+
+    file_handle = maybe_file_handle
+    file_handle.seek(offset)
+
+    # Overwrite
+    for hex_value in hex_data:
+        file_handle.write(bytes((int(hex_value),)))
+
+    file_handle.close()
+    return True
+
+
+def try_open_file(file: pathlib.Path, mode: str):
+    try:
+        return open(file, mode)
+    except PermissionError as e:
+        print(
+            f"Failed to open file {file.name}: the permision was denied. Caught the following exception:\n{e}"
+        )
+        return None
+    except Exception as e:
+        print(f"Failed to open file {file.name}. Caught the following exception:\n{e}")
+        return None
