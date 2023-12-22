f43f39205bb830c66624ba7c57596da700d6b8ea
Taeke de Haan <taeke.dehaan@live.nl>
2023-12-22 13:07:04 +0100

    [main] Change default folder to Music

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from src.core import Core
 
 
 def main():
-    core = Core(pathlib.Path.home() / "Music" / "PioneerDJ" / "Tracks")
+    # configure
+    music_path = pathlib.Path.home() / "Music"
+    path = music_path if music_path.is_dir() else pathlib.Path.home()
+
+    # construct
+    core = Core(path)
     ui = UI(core)
 
+    # run
     ui.run()
 
 
